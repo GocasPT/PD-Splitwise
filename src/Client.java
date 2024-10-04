51eb81314384a4d2e55a5ad5912f670ea6528436
GocasPT <gocasousa2013@gmail.com>
2024-10-04 17:45:19 +0100

    Init project

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+public class Client {
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Usage: java Client <server> <port>");
+			return;
+		}
+	}
+}
---
+++ src/Server.java
@@ -0,0 +1,8 @@
+public class Server {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Usage: java Server <port>");
+			return;
+		}
+	}
+}
