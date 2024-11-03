d5b745a464df6e0984703bd917490501241c37d4
GocasPT <gocasousa2013@gmail.com>
2024-11-03 16:33:25 +0000

    Terminal methods for clean prints :)

---
+++ common/src/main/java/pt/isec/a2021138502/PD_Splitwise/Terminal/utils.java
@@ -0,0 +1,20 @@
+package pt.isec.a2021138502.PD_Splitwise.Terminal;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class utils {
+	public static void printProgress(long current, long total) {
+		int percentage = (int) ((current * 100.0) / total);
+		int progressChars = (int) ((60.0 * current) / total);
+		String progress = "\r[" +
+				"=".repeat(progressChars) +
+				" ".repeat(60 - progressChars) +
+				String.format("] %d%% (%d/%d bytes)", percentage, current, total);
+		System.out.println(getTimeTag() + progress);
+	}
+
+	public static String getTimeTag() {
+		return "<" + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "> ";
+	}
+}
