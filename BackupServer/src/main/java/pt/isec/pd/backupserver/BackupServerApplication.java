18eb30bc96fabd5c03365edb9955fce06032293c
GocasPT <gocasousa2013@gmail.com>
2024-11-11 20:13:03 +0000

    Client javaFX app + Server spring boot app + Backup Server spring boot app

---
+++ BackupServer/src/main/java/pt/isec/pd/backupserver/BackupServerApplication.java
@@ -0,0 +1,13 @@
+package pt.isec.pd.backupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupServerApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/pt/isec/pd/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.isec.pd.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
